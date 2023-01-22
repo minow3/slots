48914f4c7778d5159bdb87bb0337ccccac5a9bfe
minow3 <114686250+minow3@users.noreply.github.com>
2023-01-22 11:30:45 +0000

    feat: wellcome message

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import curses
+from curses import wrapper
+
+def main (stdscr):
+  stdscr.clear()
+  stdscr.addstr(1, 0,"------------------------------------------------------------")
+  stdscr.addstr(2,20,"WELLCOME TO")
+  stdscr.addstr(3, 0,"------------------------------------------------------------")
+  stdscr.addstr(4,20, "SLOT MACHINE")
+  stdscr.addstr(5, 0,"------------------------------------------------------------")
+  stdscr.refresh()
+  stdscr.getch()
+  
+wrapper(main)
